0a897674fdb8eda1134c2a0b0f52f9a49ea916c8
BharathiMangaleswaran <157895567+BharathiMangaleswaran@users.noreply.github.com>
2024-01-27 06:08:55 -0800

    Add files via upload

---
+++ Atmmachine/src/main/java/Starting.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Starting extends Atm {
+	public static void main(String[]args) throws IOException{
+		Atm atm =new Atm();
+		atm.getLogin();
+		
+	}
+
+}
